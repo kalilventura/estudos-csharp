6369dad73064c9df618c7f79fb56eab4b8dc7f26
Kalil Teixeira Ventura Monteiro <kalilteixeira@brq.com>
2019-12-04 12:14:42 -0300

    Criando testes para exception

---
+++ TDDCurso/Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Alura.LeilaoOnline.Core;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoTerminaPregao
+    {
+        [Theory]
+        [InlineData(1200, new double[] { 800, 900, 1000, 1200 })]
+        [InlineData(1000, new double[] { 800, 900, 1000, 990 })]
+        [InlineData(800, new double[] { 800 })]
+        public void RetornaMaiorValorDadoLeilaoComPeloMenosUmLance(
+            double valorEsperado,
+            double[] ofertas)
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+            leilao.IniciaPregao();
+            for (int i = 0; i < ofertas.Length; i++)
+            {
+                var valor = ofertas[i];
+                if ((i % 2) == 0)
+                {
+                    leilao.RecebeLance(fulano, valor);
+                }
+                else
+                {
+                    leilao.RecebeLance(maria, valor);
+                }
+            }
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+
+        }
+
+        [Fact]
+        public void RetornaZeroDadoLeilaoSemLances()
+        {
+            //Arranje - cenário
+            var leilao = new Leilao("Van Gogh");
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 0;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        public void LancaInvalidOperationExceptionDadoPegaoNaoIniciado()
+        {
+            try
+            {
+                //Assert
+                var leilao = new Leilao("Van Gogh");
+
+                //Act - método sob teste
+                leilao.TerminaPregao();
+
+                // Utilizado para que caso não ocorra a exceção, a linha abaixo faz o teste não funcionar
+                Assert.True(false);
+            }
+            catch (Exception err)
+            {
+                // Verifico se a Exception é do tipo InvalidOperation
+                Assert.IsType<System.InvalidOperationException>(err);
+            }
+        }
+
+    }
+}
