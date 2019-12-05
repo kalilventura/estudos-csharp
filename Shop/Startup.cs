d91a073478355f1414596954fedf23b618fc30c8
Kalil Teixeira Ventura Monteiro <kalilteixeira@brq.com>
2019-12-05 17:37:30 -0300

    Adicionando documentacao com Swagger

--- Shop/Startup.cs
+++ Shop/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Shop.Data;
 
 namespace Shop
@@ -25,7 +26,7 @@ namespace Shop
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             // Vai comprimir antes de enviar a mensagem
             services.AddResponseCompression(options =>
             {
@@ -34,7 +35,7 @@ namespace Shop
                                             .MimeTypes
                                             .Concat(new[] { "application/json" });
             });
-            
+
             // Para Caching toda a aplicação
             // services.AddResponseCaching();
 
@@ -64,6 +65,11 @@ namespace Shop
 
             // A abertura e fechamento da conexão do banco de dados ficam pelo escopo
             services.AddScoped<DataContext, DataContext>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Shop API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -75,8 +81,19 @@ namespace Shop
             }
 
             app.UseHttpsRedirection();
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shop API v1");
+            });
 
             app.UseRouting();
+
+            app.UseCors(x => x
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin());
 
             app.UseAuthentication();
             app.UseAuthorization();
