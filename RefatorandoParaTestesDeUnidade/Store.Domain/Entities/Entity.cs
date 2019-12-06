f99c86796c74f9521d31d6d44d9e1173ecdda2d1
Kalil Teixeira Ventura Monteiro <kalilteixeira@brq.com>
2019-12-06 15:18:26 -0300

    Instalacao do Flunt e inicio do dominio com Design By Contracts

--- RefatorandoParaTestesDeUnidade/Store.Domain/Entities/Entity.cs
+++ RefatorandoParaTestesDeUnidade/Store.Domain/Entities/Entity.cs
@@ -1,8 +1,9 @@
 using System;
+using Flunt.Notifications;
 
 namespace Store.Domain.Entities
 {
-    public class Entity
+    public class Entity : Notifiable
     {
         public Guid Id {get; private set;}
         public Entity()
