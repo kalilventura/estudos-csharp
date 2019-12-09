960c09e8698daf667b2bc2869eeb768d6ed19e82
Kalil Teixeira Ventura Monteiro <kalilteixeira@brq.com>
2019-12-09 16:02:32 -0300

    Criacao dos Commands e testes para os mesmos

---
+++ RefatorandoParaTestesDeUnidade/Store.Tests/Commands/CreateOrderCommandTests.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.Commands;
+
+namespace Store.Tests.Commands
+{
+    [TestClass]
+    public class CreateOrderCommandTests
+    {
+        [TestMethod]
+        [TestCategory("Handlers")]
+        public void Dado_um_comando_invalido_o_pedido_nao_deve_ser_gerado()
+        {
+            var command = new CreateOrderCommand();
+            command.Customer = "";
+            command.ZipCode = "13411080";
+            command.PromoCode = "12345678";
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Validate();
+
+            Assert.AreEqual(command.Valid, false);
+        }
+    }
+}
