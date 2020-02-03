1aecfd6f5c462429b90130936f91a5987c432181
Kalil Teixeira Ventura Monteiro <kalilventur@gmail.com>
2020-02-02 21:59:18 -0300

    Criacao dos Commands

--- TodoApi/Todo.Domain/Commands/UpdateTodoCommand.cs
+++ TodoApi/Todo.Domain/Commands/UpdateTodoCommand.cs
@@ -5,27 +5,28 @@ using Todo.Domain.Commands.Contracts;
 
 namespace Todo.Domain.Commands
 {
-    public class CreateTodoCommand : Notifiable, ICommand
+    public class UpdateTodoCommand : Notifiable, ICommand
     {
-        public CreateTodoCommand() { }
-        public CreateTodoCommand(string title, string user, DateTime date)
+        public UpdateTodoCommand() { }
+
+        public UpdateTodoCommand(Guid id, string title, string user)
         {
+            Id = id;
             Title = title;
             User = user;
-            Date = date;
         }
 
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string User { get; set; }
-        public DateTime Date { get; set; }
 
         public void Validate()
         {
             AddNotifications(
                 new Contract()
                     .Requires()
-                    .HasMaxLen(Title, 3, "Title", "Por favor, descreva melhor essa tarefa")
-                    .HasMinLen(User, 6, "User", "Usuário Inválido")
+                    .HasMinLen(Title, 3, "Title", "Por favor, descreva melhor esta tarefa!")
+                    .HasMinLen(User, 6, "User", "Usuário inválido!")
             );
         }
     }
