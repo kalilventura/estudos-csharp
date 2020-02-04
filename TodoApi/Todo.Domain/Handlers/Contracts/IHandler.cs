ca2f4247a3b75b558b68e04e5b098a4d8f75890e
kalilventura <kalilventur@gmail.com>
2020-02-04 16:49:48 -0300

    Criacao do Handler

--- TodoApi/Todo.Domain/Handlers/Contracts/IHandler.cs
+++ TodoApi/Todo.Domain/Handlers/Contracts/IHandler.cs
@@ -1,7 +1,9 @@
+using Todo.Domain.Commands.Contracts;
+
 namespace Todo.Domain.Handlers.Contracts
 {
-    public interface IHandler
+    public interface IHandler<T> where T : ICommand
     {
-         
+        ICommandResult Handle(ICommand command);
     }
 }
