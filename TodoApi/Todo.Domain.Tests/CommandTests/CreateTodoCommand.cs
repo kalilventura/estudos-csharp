6df042bc0442f9f565fc0220ef3e9044fa4a2b7e
Kalil Teixeira Ventura Monteiro <kalilventur@gmail.com>
2020-02-04 22:24:43 -0300

    Inicio da criacao dos testes

---
+++ TodoApi/Todo.Domain.Tests/CommandTests/CreateTodoCommand.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Todo.Domain.Tests.CommandTests
+{
+    [TestClass]
+    public class CreateTodoCommand
+    {
+        [TestMethod]
+        public void Dado_um_comando_invalido()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Dado_um_comando_valido()
+        {
+            Assert.Fail();
+        }
+
+
+    }
+}
