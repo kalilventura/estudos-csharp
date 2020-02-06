43b4cc2e4be0111296567f02c798739c027343d4
Kalil Teixeira Ventura Monteiro <kalilventur@gmail.com>
2020-02-05 21:51:21 -0300

    Testes

---
+++ TodoApi/Todo.Domain.Tests/Queries/TodoQueryTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Todo.Domain.Entities;
+using Todo.Domain.Queries;
+
+namespace Todo.Domain.Tests.QueriesTests
+{
+    [TestClass]
+    public class TodoQueryTests
+    {
+        private List<TodoItem> _items;
+
+        public TodoQueryTests()
+        {
+            _items = new List<TodoItem>();
+            _items.Add(new TodoItem("Tarefa 1", DateTime.Now, "usuarioA"));
+            _items.Add(new TodoItem("Tarefa 2", DateTime.Now, "usuarioA"));
+            _items.Add(new TodoItem("Tarefa 3", DateTime.Now, "andrebaltieri"));
+            _items.Add(new TodoItem("Tarefa 4", DateTime.Now, "usuarioA"));
+            _items.Add(new TodoItem("Tarefa 5", DateTime.Now, "andrebaltieri"));
+        }
+
+        [TestMethod]
+        public void Dada_a_consulta_deve_retornar_tarefas_apenas_do_usuario_andrebaltieri()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetAll("andrebaltieri"));
+            Assert.AreEqual(2, result.Count());
+        }
+    }
+}
