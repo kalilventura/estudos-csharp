69307bfe1067d69f8c4a72ea1c5391313eea7869
Kalil Teixeira Ventura Monteiro <kalilventur@gmail.com>
2020-02-10 21:33:04 -0300

    Explicacao Design Chain of responsability

--- CursoDesignPatterns/Chain_of_Responsability/Program.cs
+++ CursoDesignPatterns/Chain_of_Responsability/Program.cs
@@ -12,6 +12,14 @@ namespace Chain_of_Responsability
 
             // Condição para implementação: Quando temos várias regras de negócio e temos condições para
             // aplicar cada um delas
+			
+			/*
+			A ideia do padrão é resolver problemas como esses: de acordo com o cenário, 
+			devemos realizar alguma ação. Ao invés de escrevermos código procedural, 
+			e deixarmos um único método descobrir o que deve ser feito, 
+			quebramos essas responsabilidades em várias diferentes classes, 
+			e as unimos como uma corrente.
+			*/
             var calculadorDescontos = new CalculadorDeDescontos();
             var orcamento = new Orcamento(500);
 
